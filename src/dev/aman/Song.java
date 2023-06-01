72e549a98af4e92ce5cdbda3084a05c9aa810728
amtenuJava <java.amanuel@gmail.com>
2023-06-01 09:40:01 -0600

    New feature
    user can set and get song title and duration.

---
+++ src/dev/aman/Song.java
@@ -0,0 +1,33 @@
+package dev.aman;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+
+    public String getName() {
+        return title;
+    }
+
+    public void setName(String name) {
+        this.title = name;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": "+ this.duration;
+    }
+}
